66d346a4062d42cdb48e11aef04ffb00edd82efe
Greg Anderson <greg@infobytes.com>
2015-10-19 10:15:12 -0600

    Now storing the string value of the background color.

--- app/src/main/java/com/familybiz/greg/learncolors/ColorActivity.java
+++ app/src/main/java/com/familybiz/greg/learncolors/ColorActivity.java
@@ -3,6 +3,7 @@ package com.familybiz.greg.learncolors;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,27 +13,29 @@ import android.widget.TextView;
 
 public class ColorActivity extends Activity {
 
+	private Pair mCurrentColor;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		final LinearLayout rootLayout = new LinearLayout(this);
 		setTheme(android.R.style.Theme_Holo_Light);
 
-		final Integer[] colorArray = {
-				Color.BLUE,
-				Color.BLACK,
-				Color.RED,
-				Color.GREEN,
-				Color.YELLOW,
-				Color.WHITE,
-				Color.GRAY,
-				Color.rgb(255, 102, 0),		// orange
-				Color.rgb(128, 0, 128),		// purple
-				Color.rgb(92, 51, 23),		// brown
-				Color.rgb(255, 192, 203)	// pink
+		final Pair[] colorArray = {
+				new Pair(Color.BLUE, "blue"),
+				new Pair(Color.BLACK, "black"),
+				new Pair(Color.RED, "red"),
+				new Pair(Color.GREEN, "green"),
+				new Pair(Color.YELLOW, "yellow"),
+				new Pair(Color.WHITE, "white"),
+				new Pair(Color.GRAY, "gray"),
+				new Pair(Color.rgb(255, 102, 0), "orange"),
+				new Pair(Color.rgb(128, 0, 128), "purple"),
+				new Pair(Color.rgb(92, 51, 23), "brown"),
+				new Pair(Color.rgb(255, 192, 203), "pink")
 		};
 
-		final RandomItemList<Integer> colors = new RandomItemList<Integer>(colorArray);
+		final RandomItemList<Pair> colors = new RandomItemList<Pair>(colorArray);
 
 		final TextView screenText = new TextView(this);
 		screenText.setText(R.string.screenText);
@@ -40,12 +43,15 @@ public class ColorActivity extends Activity {
 		screenText.setGravity(Gravity.CENTER);
 
 		// Set initial color
-		screenText.setBackgroundColor(colors.nextItem());
+		mCurrentColor = colors.nextItem();
+		screenText.setBackgroundColor(mCurrentColor.key);
 
 		screenText.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				screenText.setBackgroundColor(colors.nextItem());
+				mCurrentColor = colors.nextItem();
+				screenText.setBackgroundColor(mCurrentColor.key);
+				Log.i("Color", mCurrentColor.value);
 			}
 		});
 
@@ -53,5 +59,14 @@ public class ColorActivity extends Activity {
 		rootLayout.addView(screenText, screenTextParams);
 
 		setContentView(rootLayout);
+	}
+
+	class Pair {
+		Integer key;
+		String value;
+		public Pair(Integer key, String value) {
+			this.key = key;
+			this.value = value;
+		}
 	}
 }
